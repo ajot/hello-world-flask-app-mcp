cde6df6815808a214dfc697d935de3dcab8d53f2
Amit Jotwani <hello@ajot.me>
2025-04-18 15:35:00 -0400

    Add /joke endpoint with Chuck Norris jokes

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify
 import os
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/joke')
+def get_joke():
+    response = requests.get('https://api.chucknorris.io/jokes/random')
+    if response.status_code == 200:
+        joke_data = response.json()
+        return jsonify({
+            'joke': joke_data['value'],
+            'categories': joke_data['categories'],
+            'id': joke_data['id']
+        })
+    else:
+        return jsonify({'error': 'Failed to fetch joke'}), 500
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 8080))
